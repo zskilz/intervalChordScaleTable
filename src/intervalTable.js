f5da57e87a1ea8b67dbadbc6457ed939c6f5abe0
Petrus J. Pretorius <zskilz@gmail.com>
2013-05-01 04:02:29 -0400

    Style updates

--- src/intervalTable.js
+++ src/intervalTable.js
@@ -45,19 +45,21 @@
 
                 if (!data) {
                     // Setup
+                    var intervalTable = $('<div/>').addClass('intervalTable');
                     var dominant = $('<div/>').addClass('dominant').html('Dominant Note:');
-                    var intervalTable = $('<table/>').addClass('intervalTable');
+                    var theTable = $('<table/>');
                     var heading = $('<tr><th rowspan="2">Note Number</th><th rowspan="2">Pitch</th><th rowspan="2">Interval name</th></tr>');
                     var chordsScales = $('<tr/>').addClass('chordsScales');
 
                     //add to dom
-                    intervalTable.append(heading);
-                    intervalTable.append(chordsScales);
+                    theTable.append(heading);
+                    theTable.append(chordsScales);
 
-                    $this.append(dominant);
-                    $this.append(intervalTable);
-                    $this.append($('<div/>').addClass('legend').html(_opt.legend));
+                    intervalTable.append(dominant);
+                    intervalTable.append(theTable);
+                    intervalTable.append($('<div/>').addClass('legend').html(_opt.legend));
 
+                    $this.append(intervalTable)
                     //setup logic
 
                     var dominantSelect = $('<select/>');
@@ -69,7 +71,7 @@
 
                     dominantSelect.change(function() {
                         currKeyIndex = _mh.SciNotes.indexOf($(this).val());
-                        var rows = intervalTable.find('tr');
+                        var rows = theTable.find('tr');
                         rows.removeClass('darkRow');
                         $.each(rows, function(ind, row) {
                             if (ind > 1) {
@@ -136,7 +138,7 @@
                             row.append(N);
                         })
 
-                        intervalTable.append(row);
+                        theTable.append(row);
                     }
                     //add audio support to Table 1.
 
